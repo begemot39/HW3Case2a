b64240ac1133aad55156164fe22811b6825efe78
Vitaliy K. <begemot39@mail.ru>
2024-08-21 11:18:15 +0300

    Edits1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         BmiService bmi = new BmiService();
         double kilograms = 98;
         double meters = 1.87;
-        double userBmi = bmi.calculate( kilograms, meters );
-        System.out.println( "Ваш индекс bmi равен: " + ( int ) userBmi );
+        double userBmi = bmi.calculate(kilograms, meters);
+        System.out.println("Ваш индекс bmi равен: " + (int) userBmi);
     }
 }
